32ccb7f190a19be85dabc889af8e5b61f2409679
sasidharan01 <sasidharanofficial@gmail.com>
2023-04-01 14:08:08 +0100

    updated transaction-average.py

--- PartA/transactions-average.py
+++ PartA/transactions-average.py
@@ -1,8 +1,5 @@
-import sys, string
 import os
-import socket
 import time
-import operator
 import boto3
 import json
 from pyspark.sql import SparkSession
@@ -10,9 +7,11 @@ from datetime import datetime
 
 if __name__ == "__main__":
 
+    # Initialize spark session
     spark = SparkSession.builder.appName("Ethereum").getOrCreate()
 
-    def check_transaction(line):
+    # Check the format of transactions data
+    def verify_transaction(line):
         try:
             fields = line.split(",")
             if len(fields) != 15:
@@ -23,14 +22,14 @@ if __name__ == "__main__":
         except:
             return False
 
-    # shared read-only object bucket containing datasets
+    # Fetch S3 environment variables
     s3_data_repository_bucket = os.environ["DATA_REPOSITORY_BUCKET"]
-
     s3_endpoint_url = os.environ["S3_ENDPOINT_URL"] + ":" + os.environ["BUCKET_PORT"]
     s3_access_key_id = os.environ["AWS_ACCESS_KEY_ID"]
     s3_secret_access_key = os.environ["AWS_SECRET_ACCESS_KEY"]
     s3_bucket = os.environ["BUCKET_NAME"]
 
+    # Configure Hadoop settings for the Spark session
     hadoopConf = spark.sparkContext._jsc.hadoopConfiguration()
     hadoopConf.set("fs.s3a.endpoint", s3_endpoint_url)
     hadoopConf.set("fs.s3a.access.key", s3_access_key_id)
@@ -46,31 +45,45 @@ if __name__ == "__main__":
         vals = float(value)
         return (date, (vals, 1))
 
-    t = spark.sparkContext.textFile(
+    # Fetch transactions.csv file from S3 bucket
+    transactions = spark.sparkContext.textFile(
         "s3a://"
         + s3_data_repository_bucket
         + "/ECS765/ethereum-parvulus/transactions.csv"
     )
-    clean_lines = t.filter(check_transaction)
-    date_vals = clean_lines.map(mapping)
-    reducing = date_vals.reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
-    avg_transactions = reducing.map(lambda a: (a[0], str(a[1][0] / a[1][1])))
-    avg_transactions = avg_transactions.map(lambda op: ",".join(str(tr) for tr in op))
 
-    my_bucket_resource = boto3.resource(
+    # Filter transactions data
+    transactions_filtered = transactions.filter(verify_transaction)
+    
+    # Map transactions data to extract features and aggregate values for average transaction value calculation
+    transactions_mapped = transactions_filtered.map(mapping)
+    
+    # Reduce transactions data by date to calculate average transaction value per month
+    transactions_reduced = transactions_mapped.reduceByKey(
+        lambda x, y: (x[0] + y[0], x[1] + y[1])
+    )
+    
+    transactions_avg = transactions_reduced.map(
+        lambda a: (a[0], str(a[1][0] / a[1][1]))
+    )
+    transactions_avg = transactions_avg.map(lambda op: ",".join(str(tr) for tr in op))
+
+    # Create resource object for S3 bucket
+    bucket = boto3.resource(
         "s3",
         endpoint_url="http://" + s3_endpoint_url,
         aws_access_key_id=s3_access_key_id,
         aws_secret_access_key=s3_secret_access_key,
     )
 
-    now = datetime.now()  # current date and time
+    # Store results in S3 bucket
+    now = datetime.now()
     date_time = now.strftime("%d-%m-%Y_%H:%M:%S")
 
-    my_result_object = my_bucket_resource.Object(
-        s3_bucket, "ethereum_avg" + date_time + "/transactions_avg.txt"
+    obj = bucket.Object(
+        s3_bucket, "ethereum_avg_" + date_time + "/transactions_avg.txt"
     )
 
-    my_result_object.put(Body=json.dumps(avg_transactions.take(100)))
+    obj.put(Body=json.dumps(transactions_avg.take(100)))
 
     spark.stop()

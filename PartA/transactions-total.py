3dd7857df800a3a7037c39b84235d1974885c5b5
sasidharan01 <sasidharanofficial@gmail.com>
2023-04-01 13:55:42 +0100

    modified transactions-total.py

--- PartA/transactions-total.py
+++ PartA/transactions-total.py
@@ -1,6 +1,4 @@
-import sys, string
 import os
-import socket
 import time
 import operator
 import boto3
@@ -10,9 +8,11 @@ from datetime import datetime
 
 if __name__ == "__main__":
 
+    # Initialize spark session
     spark = SparkSession.builder.appName("Ethereum").getOrCreate()
 
-    def check_transaction(line):
+    # Check the format of transactions data
+    def verify_transaction(line):
         try:
             fields = line.split(",")
             if len(fields) != 15:
@@ -22,14 +22,14 @@ if __name__ == "__main__":
         except:
             return False
 
-    # shared read-only object bucket containing datasets
+    # Fetch S3 environment variables
     s3_data_repository_bucket = os.environ["DATA_REPOSITORY_BUCKET"]
-
     s3_endpoint_url = os.environ["S3_ENDPOINT_URL"] + ":" + os.environ["BUCKET_PORT"]
     s3_access_key_id = os.environ["AWS_ACCESS_KEY_ID"]
     s3_secret_access_key = os.environ["AWS_SECRET_ACCESS_KEY"]
     s3_bucket = os.environ["BUCKET_NAME"]
 
+    # Configure Hadoop settings for the Spark session
     hadoopConf = spark.sparkContext._jsc.hadoopConfiguration()
     hadoopConf.set("fs.s3a.endpoint", s3_endpoint_url)
     hadoopConf.set("fs.s3a.access.key", s3_access_key_id)
@@ -37,33 +37,40 @@ if __name__ == "__main__":
     hadoopConf.set("fs.s3a.path.style.access", "true")
     hadoopConf.set("fs.s3a.connection.ssl.enabled", "false")
 
-    t = spark.sparkContext.textFile(
+    # Fetch transactions.csv file from S3 bucket
+    transactions = spark.sparkContext.textFile(
         "s3a://"
         + s3_data_repository_bucket
         + "/ECS765/ethereum-parvulus/transactions.csv"
     )
-    clean_lines = t.filter(check_transaction)
 
-    transactions = clean_lines.map(
+    # Filter transactions data
+    transactions_filtered = transactions.filter(verify_transaction)
+
+    """
+    Extract the month and year from the block_timestamp field 
+    and map it to 1 for each transaction
+    """
+    transactions_mapped = transactions_filtered.map(
         lambda b: (time.strftime("%m/%Y", time.gmtime(int(b.split(",")[11]))), 1)
     )
-    tot_transactions = transactions.reduceByKey(operator.add)
 
-    my_bucket_resource = boto3.resource(
+    # Reduce by key (month and year) to get the total number of transactions per month
+    total_transactions = transactions_mapped.reduceByKey(operator.add)
+
+    # Create resource object for S3 bucket
+    bucket = boto3.resource(
         "s3",
         endpoint_url="http://" + s3_endpoint_url,
         aws_access_key_id=s3_access_key_id,
         aws_secret_access_key=s3_secret_access_key,
     )
 
+    # Store results in S3 bucket
     now = datetime.now()  # current date and time
     date_time = now.strftime("%d-%m-%Y_%H:%M:%S")
 
-    my_result_object = my_bucket_resource.Object(
-        s3_bucket, "ethereum_" + date_time + "/transactions_total.txt"
-    )
-    my_result_object.put(Body=json.dumps(tot_transactions.take(100)))
-
-    print(tot_transactions.take(100))
+    obj = bucket.Object(s3_bucket, "ethereum_" + date_time + "/transactions_total.txt")
+    obj.put(Body=json.dumps(total_transactions.take(100)))
 
     spark.stop()

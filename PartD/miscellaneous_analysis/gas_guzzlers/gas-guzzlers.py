3569c8d67739f520e072807bdaeb8ce8434ef368
sasidharan01 <sasidharanofficial@gmail.com>
2023-04-04 16:15:38 +0100

    added popular scam code for plot generation

--- PartD/miscellaneous_analysis/gas_guzzlers/gas-guzzlers.py
+++ PartD/miscellaneous_analysis/gas_guzzlers/gas-guzzlers.py
@@ -1,4 +1,3 @@
-
 import sys, string
 import os
 import socket
@@ -12,40 +11,36 @@ from pyspark.sql.functions import from_unixtime, month, year
 
 if __name__ == "__main__":
 
-    spark = SparkSession\
-        .builder\
-        .appName("Olympic")\
-        .getOrCreate()
+    spark = SparkSession.builder.appName("Olympic").getOrCreate()
 
     def good_line_tra(line):
         try:
-            fields = line.split(',')
-            if len(fields)!=15:
+            fields = line.split(",")
+            if len(fields) != 15:
                 return False
             int(fields[11])
             float(fields[9])
             return True
         except:
             return False
-        
+
     def good_line_con(line):
         try:
-            fields = line.split(',')
-            if len(fields)!=6:
+            fields = line.split(",")
+            if len(fields) != 6:
                 return False
             bool(fields[3])
             return True
         except:
             return False
-    
 
     # shared read-only object bucket containing datasets
-    s3_data_repository_bucket = os.environ['DATA_REPOSITORY_BUCKET']
+    s3_data_repository_bucket = os.environ["DATA_REPOSITORY_BUCKET"]
 
-    s3_endpoint_url = os.environ['S3_ENDPOINT_URL']+':'+os.environ['BUCKET_PORT']
-    s3_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
-    s3_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
-    s3_bucket = os.environ['BUCKET_NAME']
+    s3_endpoint_url = os.environ["S3_ENDPOINT_URL"] + ":" + os.environ["BUCKET_PORT"]
+    s3_access_key_id = os.environ["AWS_ACCESS_KEY_ID"]
+    s3_secret_access_key = os.environ["AWS_SECRET_ACCESS_KEY"]
+    s3_bucket = os.environ["BUCKET_NAME"]
 
     hadoopConf = spark.sparkContext._jsc.hadoopConfiguration()
     hadoopConf.set("fs.s3a.endpoint", s3_endpoint_url)
@@ -53,93 +48,120 @@ if __name__ == "__main__":
     hadoopConf.set("fs.s3a.secret.key", s3_secret_access_key)
     hadoopConf.set("fs.s3a.path.style.access", "true")
     hadoopConf.set("fs.s3a.connection.ssl.enabled", "false")
-    
-    my_bucket_resource = boto3.resource('s3',
-            endpoint_url='http://' + s3_endpoint_url,
-            aws_access_key_id=s3_access_key_id,
-            aws_secret_access_key=s3_secret_access_key)
 
-    
-    lines_con = spark.sparkContext.textFile("s3a://" + s3_data_repository_bucket + "/ECS765/ethereum-parvulus/contracts.csv")    
-    # For testing small data set uncomment below lines and comment above line  
+    my_bucket_resource = boto3.resource(
+        "s3",
+        endpoint_url="http://" + s3_endpoint_url,
+        aws_access_key_id=s3_access_key_id,
+        aws_secret_access_key=s3_secret_access_key,
+    )
+
+    lines_con = spark.sparkContext.textFile(
+        "s3a://" + s3_data_repository_bucket + "/ECS765/ethereum-parvulus/contracts.csv"
+    )
+    # For testing small data set uncomment below lines and comment above line
     # lines_con = spark.sparkContext.textFile("s3a://" + s3_bucket + "/ethereum" + "/contracts_sample.csv")
     clean_lines_con = lines_con.filter(good_line_con)
-    address_con = clean_lines_con.map(lambda l: (l.split(',')[0], 1))
+    address_con = clean_lines_con.map(lambda l: (l.split(",")[0], 1))
     # (address, 1)
-    
-    lines_tra = spark.sparkContext.textFile("s3a://" + s3_data_repository_bucket + "/ECS765/ethereum-parvulus/transactions.csv")    
-    # For testing small data set uncomment below lines and comment above line  
+
+    lines_tra = spark.sparkContext.textFile(
+        "s3a://"
+        + s3_data_repository_bucket
+        + "/ECS765/ethereum-parvulus/transactions.csv"
+    )
+    # For testing small data set uncomment below lines and comment above line
     # lines_tra = spark.sparkContext.textFile("s3a://" + s3_bucket + "/ethereum" + "/transactions_sample.csv")
     clean_lines_tra = lines_tra.filter(good_line_tra)
-    address_tra = clean_lines_tra.map(lambda l: (l.split(',')[6], (time.strftime("%m/%Y",time.gmtime(int(l.split(',')[11]))),float(l.split(',')[7]),float(l.split(',')[8]),float(l.split(',')[9]))))
+    address_tra = clean_lines_tra.map(
+        lambda l: (
+            l.split(",")[6],
+            (
+                time.strftime("%m/%Y", time.gmtime(int(l.split(",")[11]))),
+                float(l.split(",")[7]),
+                float(l.split(",")[8]),
+                float(l.split(",")[9]),
+            ),
+        )
+    )
     # (address, (time, value, gas, gas_price))
-    
+
     con_tra_joined = address_con.join(address_tra)
-    
-    print('****************************'*50, con_tra_joined.take(10))
-    
+
+    print("****************************" * 50, con_tra_joined.take(10))
+
     # [('0x6baf48e1c966d16559ce2dddb616ffa72004851e', (1, ('08/2015', 5000000000000000.0, 21000.0, 500000000000.0))), ('0x6baf48e1c966d16559ce2dddb616ffa72004851e', (1, ('08/2015', 5000000000000000.0, 21000.0, 500000000000.0))), ('0x6baf48e1c966d16559ce2dddb616ffa72004851e', (1, ('08/2015', 5000000000000000.0, 21000.0, 500000000000.0))), ('0x6baf48e1c966d16559ce2dddb616ffa72004851e', (1, ('08/2015', 5000000000000000.0, 21000.0, 500000000000.0))), ('0x6baf48e1c966d16559ce2dddb616ffa72004851e', (1, ('08/2015', 5000000000000000.0, 21000.0, 500000000000.0))), ('0x6baf48e1c966d16559ce2dddb616ffa72004851e', (1, ('08/2015', 5000000000000000.0, 21000.0, 500000000000.0))), ('0x6baf48e1c966d16559ce2dddb616ffa72004851f', (1, ('08/2015', 5000000000000000.0, 21000.0, 500000000000.0))), ('0x6baf48e1c966d16559ce2dddb616ffa72004851f', (1, ('08/2015', 5000000000000000.0, 21000.0, 500000000000.0)))]
 
-    
-    
     time_gas_price_map = con_tra_joined.map(lambda l: (l[1][1][0], l[1][1][3]))
-    
+
     # print('time_gas_price_map----------------------------'*20, time_gas_price_map.collect())
     # (time, gas_price)
     # [('08/2015', 500000000000.0), ('08/2015', 500000000000.0), ('08/2015', 500000000000.0), ('08/2015', 500000000000.0), ('08/2015', 500000000000.0), ('08/2015', 500000000000.0), ('08/2015', 500000000000.0), ('08/2015', 500000000000.0)]
     time_gas_price_reduce = time_gas_price_map.reduceByKey(operator.add)
-    
-    print('time_gas_price_reduce----------------------------'*20, time_gas_price_reduce.collect())
+
+    print(
+        "time_gas_price_reduce----------------------------" * 20,
+        time_gas_price_reduce.collect(),
+    )
     # [('08/2015', 4000000000000.0)]
-    
-    my_result_object = my_bucket_resource.Object(s3_bucket,'ethereum' + '/partdgasguzzlers_time_gas_price.txt')
+
+    my_result_object = my_bucket_resource.Object(
+        s3_bucket, "ethereum" + "/partdgasguzzlers_time_gas_price.txt"
+    )
     my_result_object.put(Body=json.dumps(time_gas_price_reduce.collect()))
-    
-    
-    
-    
+
     time_gas_map = con_tra_joined.map(lambda l: (l[1][1][0], l[1][1][2]))
-    
+
     # print('time_gas_map?????????????????????????????????'*20, time_gas_map.collect())
     # (time, gas)
     # [('08/2015', 21000.0), ('08/2015', 21000.0), ('08/2015', 21000.0), ('08/2015', 21000.0), ('08/2015', 21000.0), ('08/2015', 21000.0), ('08/2015', 21000.0), ('08/2015', 21000.0)]
     time_gas_reduce = time_gas_map.reduceByKey(operator.add)
-    
-    print('time_gas_reduce???????????????????????????????'*20, time_gas_reduce.collect())
-    # [('08/2015', 168000.0)] 
-    
-    my_result_object = my_bucket_resource.Object(s3_bucket,'ethereum' + '/partdgasguzzlers_time_gas.txt')
+
+    print(
+        "time_gas_reduce???????????????????????????????" * 20, time_gas_reduce.collect()
+    )
+    # [('08/2015', 168000.0)]
+
+    my_result_object = my_bucket_resource.Object(
+        s3_bucket, "ethereum" + "/partdgasguzzlers_time_gas.txt"
+    )
     my_result_object.put(Body=json.dumps(time_gas_reduce.collect()))
-    
-    
-    
-    
+
     time_value_map = con_tra_joined.map(lambda l: (l[1][1][0], l[1][1][1]))
-    
+
     # print('time_value_map+++++++++++++++++++++++++++++++++++++'*20, time_value_map.collect())
     # (time, value)
-    # [('08/2015', 5000000000000000.0), ('08/2015', 5000000000000000.0), ('08/2015', 5000000000000000.0), ('08/2015', 5000000000000000.0), ('08/2015', 5000000000000000.0), ('08/2015', 5000000000000000.0), ('08/2015', 5000000000000000.0), ('08/2015', 5000000000000000.0)]
+    # [('08/2015', 5000000000000000.0), ('08/2015', 5000000000000000.0), ('08/2015', 5000000000000000.0), ('08/2015', 5000000000000000.('08/2015', 5000000000000000.0), ('08/2015', 5000000000000000.0), ('08/2015', 5000000000000000.0), ('08/2015', 5000000000000000.0)]
     time_value_reduce = time_value_map.reduceByKey(operator.add)
-    
-    print('time_value_reduce++++++++++++++++++++++++++++++++++'*20, time_value_reduce.collect())
-    # [('08/2015', 4e+16)]     
-    
-    my_result_object = my_bucket_resource.Object(s3_bucket,'ethereum' + '/partdgasguzzlers_time_value.txt')
-    my_result_object.put(Body=json.dumps(time_value_reduce.collect()))  
-    
-    
-    time_gas_price_count_map = con_tra_joined.map(lambda l: ('Average', (l[1][1][3], 1)))
-    
+
+    print(
+        "time_value_reduce++++++++++++++++++++++++++++++++++" * 20,
+        time_value_reduce.collect(),
+    )
+    # [('08/2015', 4e+16)]
+
+    my_result_object = my_bucket_resource.Object(
+        s3_bucket, "ethereum" + "/partdgasguzzlers_time_value.txt"
+    )
+    my_result_object.put(Body=json.dumps(time_value_reduce.collect()))
+
+    time_gas_price_count_map = con_tra_joined.map(
+        lambda l: ("Average", (l[1][1][3], 1))
+    )
+
     # print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&'*40, time_gas_price_count_map.collect())
-    
+
     # [('Average', (500000000000.0, 1)), ('Average', (500000000000.0, 1)), ('Average', (500000000000.0, 1)), ('Average', (500000000000.0, 1)), ('Average', (500000000000.0, 1)), ('Average', (500000000000.0, 1)), ('Average', (500000000000.0, 1)), ('Average', (500000000000.0, 1))]
-    
-    average_gas_price = time_gas_price_count_map.reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1])).map(lambda x: (x[0],x[1][0] / x[1][1]))
-    
-    print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'*30, average_gas_price.collect())
-    
-    my_result_object = my_bucket_resource.Object(s3_bucket,'ethereum' + '/partdgasguzzlers_average_gas_price.txt')
+
+    average_gas_price = time_gas_price_count_map.reduceByKey(
+        lambda x, y: (x[0] + y[0], x[1] + y[1])
+    ).map(lambda x: (x[0], x[1][0] / x[1][1]))
+
+    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" * 30, average_gas_price.collect())
+
+    my_result_object = my_bucket_resource.Object(
+        s3_bucket, "ethereum" + "/partdgasguzzlers_average_gas_price.txt"
+    )
     my_result_object.put(Body=json.dumps(average_gas_price.collect()))
-    
-    
+
     spark.stop()
---
+++ PartD/scam_analysis/popular_scams/scam-temp.py
@@ -0,0 +1,123 @@
+import os
+import time
+import operator
+import boto3
+import json
+from pyspark.sql import SparkSession
+from datetime import datetime
+
+if __name__ == "__main__":
+
+    spark = SparkSession.builder.appName("Ethereum").getOrCreate()
+
+    def verify_transactions(line):
+        try:
+            fields = line.split(",")
+            if len(fields) != 15:
+                return False
+            int(fields[11])
+            float(fields[9])
+            return True
+        except:
+            return False
+
+    def verify_scams(line):
+        try:
+            fields = line.split(",")
+            if len(fields) != 8:
+                return False
+            int(fields[0])
+            return True
+        except:
+            return False
+
+    s3_data_repository_bucket = os.environ["DATA_REPOSITORY_BUCKET"]
+    s3_endpoint_url = os.environ["S3_ENDPOINT_URL"] + \
+        ":" + os.environ["BUCKET_PORT"]
+    s3_access_key_id = os.environ["AWS_ACCESS_KEY_ID"]
+    s3_secret_access_key = os.environ["AWS_SECRET_ACCESS_KEY"]
+    s3_bucket = os.environ["BUCKET_NAME"]
+
+    hadoopConf = spark.sparkContext._jsc.hadoopConfiguration()
+    hadoopConf.set("fs.s3a.endpoint", s3_endpoint_url)
+    hadoopConf.set("fs.s3a.access.key", s3_access_key_id)
+    hadoopConf.set("fs.s3a.secret.key", s3_secret_access_key)
+    hadoopConf.set("fs.s3a.path.style.access", "true")
+    hadoopConf.set("fs.s3a.connection.ssl.enabled", "false")
+
+    scams = spark.sparkContext.textFile(
+        "s3a://"
+        + s3_data_repository_bucket
+        + "/ECS765/ethereum-parvulus/scams.csv"
+    )
+    transactions = spark.sparkContext.textFile(
+        "s3a://"
+        + s3_data_repository_bucket
+        + "/ECS765/ethereum-parvulus/transactions.csv"
+    )
+
+    scams_filtered = scams.filter(verify_scams)
+    scams_transformed = scams_filtered.map(
+        lambda x: (x.split(",")[6], (x.split(",")[0],
+                   x.split(",")[4], x.split(",")[7]))
+    )
+
+    transactions_filtered = transactions.filter(verify_transactions)
+    transactions_transformed = transactions_filtered.map(
+        lambda x: (
+            x.split(",")[6],
+            (
+                time.strftime("%m/%Y", time.gmtime(int(x.split(",")[11]))),
+                float(x.split(",")[7]),
+            ),
+        )
+    )
+    transactions_scams_transformed = transactions_transformed.join(
+        scams_transformed)
+
+    category_status_mapping = transactions_scams_transformed.map(
+        lambda x: ((x[1][1][1], x[1][1][2], x[1][0][0]), x[1][0][1])
+    )
+    category_status_reduced = category_status_mapping.reduceByKey(operator.add)
+
+    category_mapping = transactions_scams_transformed.map(
+        lambda x: ((x[1][1][1], x[1][0][0]), x[1][0][1]))
+    category_reduced = category_mapping.reduceByKey(operator.add)
+
+    scam_id_mapping = transactions_scams_transformed.map(
+        lambda x: (x[1][1][0], x[1][0][1]))
+    scam_id_reduced = scam_id_mapping.reduceByKey(
+        operator.add).sortBy(lambda a: -a[1])
+
+    bucket = boto3.resource(
+        "s3",
+        endpoint_url="http://" + s3_endpoint_url,
+        aws_access_key_id=s3_access_key_id,
+        aws_secret_access_key=s3_secret_access_key,
+    )
+
+    # Store results in S3 bucket
+    now = datetime.now()
+    date_time = now.strftime("%d-%m-%Y_%H:%M:%S")
+
+    obj = bucket.Object(
+        s3_bucket, "ethereum_scam_analysis_" +
+        date_time + "/ether_category_status_time.txt"
+    )
+    obj.put(Body=json.dumps(category_status_reduced.collect()))
+
+    obj = bucket.Object(
+        s3_bucket, "ethereum_scam_analysis_" + 
+        date_time +
+        "/ether_category_time.txt"
+    )
+    obj.put(Body=json.dumps(category_reduced.collect()))
+
+    obj = bucket.Object(
+        s3_bucket, "ethereum_scam_analysis_" +
+        date_time +
+        "/popular_scams.txt"
+    )
+    obj.put(Body=json.dumps(scam_id_reduced.collect()))
+
+    spark.stop()
